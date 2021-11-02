02b34e4fd26b56e8404992afa422cc630ddfd601
Álvaro Miguel <alvaro.lorente@nodrizatech.com>
2021-11-02 10:25:47 +0100

    Add api-platform dependency

--- src/Entity/Tareas.php
+++ src/Entity/Tareas.php
@@ -3,10 +3,12 @@
 namespace App\Entity;
 
 use App\Repository\TareasRepository;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=TareasRepository::class)
+ * @ApiResource()
  */
 class Tareas
 {
