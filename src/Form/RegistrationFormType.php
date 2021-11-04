6a86647a810eb4baabc3a54942badaea1af31001
Álvaro Miguel <alvaro.lorente@nodrizatech.com>
2021-11-04 10:37:34 +0100

    Modify database schema and add mailer url and redirect to admin

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,18 +20,18 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
-                'constraints' => [
+                /*'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
-            ])
+            */])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
                 'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
+                /*'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
                     ]),
@@ -42,7 +42,7 @@ class RegistrationFormType extends AbstractType
                         'max' => 4096,
                     ]),
                 ],
-            ])
+            */])
         ;
     }
 
