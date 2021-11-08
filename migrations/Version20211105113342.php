1089782470ac18af9849d34cb24d920cd444d92b
Álvaro Miguel <alvaro.lorente@nodrizatech.com>
2021-11-08 10:20:14 +0100

    Add user association with tareas

--- migrations/Version20211105113342.php
+++ migrations/Version20211105113342.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20211102083710 extends AbstractMigration
+final class Version20211105113342 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20211102083710 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE tareas ADD categoria VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE tareas ADD creada DATETIME NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE tareas DROP categoria');
+        $this->addSql('ALTER TABLE tareas DROP creada');
     }
 }
