d40cd7c5cf68274c6f9cd7b5401eb1260b6de628
Álvaro Miguel <alvaro.lorente@nodrizatech.com>
2021-11-29 11:21:09 +0100

    Add new controller to control the task tranfer form

--- src/Controller/TransferirTareasController.php
+++ src/Controller/TransferirTareasController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Tareas;
 use App\Entity\User;
 use App\Form\TaskTransferFormType;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,30 +15,89 @@ use Symfony\Component\Routing\Annotation\Route;
 class TransferirTareasController extends AbstractController
 {
     /**
-     * @Route("/transferir/tareas", name="transferir_tareas")
+     * @Route("/taskTransfer/{id}/{end}", name="taskTransfer")
      */
-    public function new(Request $request): Response
+    public function transfer(Request $request): Response
     {
-
         $form = $this->createForm(TaskTransferFormType::class);
 
         if ($request->isMethod('POST')) {
 
             $form->submit($request->request->get($form->getName()));
+
+
             if ($form->isSubmitted() && $form->isValid()) {
-                // $form->getData() holds the submitted values
-                // but, the original `$task` variable has also been updated
+
                 $user = $form->getData();
 
-                // ... perform some action, such as saving the task to the database
-                // for example, if Task is a Doctrine entity, save it!
-                $entityManager = $this->getDoctrine()->getManager()->getRepository('App:User')->findOneBy(array('username' => $user['username']));
+                $comprobar = $this->getDoctrine()->getManager()->getRepository('App:User')->findOneBy(array('email' => $user['username']));
 
-                return $this->redirectToRoute('admin');
+                if ($comprobar != NULL) {
+
+                    // Obteniendo el id del usuario:
+
+                    $id = $comprobar->getId();
+
+                    // Hago esta consulta: SELECT count(*) FROM tareas WHERE id_usuario_id = 1 AND realizada = 0;
+
+                    $q = $this->getDoctrine()->getManager()->getRepository('App:Tareas')->createQueryBuilder('tareas')
+                        ->select('count(tareas.id)')
+                        ->where('tareas.user = :id')
+                        ->andWhere('tareas.realizada = false')
+                        ->setParameter('id', $id)
+                        ->getQuery()->getResult();
+
+                    // Asigno a $q la cantidad de tareas sin completar que tiene el usuario
+
+                    $q = $q[0][1];
+
+                    if ($user['username'] === $comprobar->getUsername() && $request->attributes->get('end') == 'false' && $q < 3) {
+
+                        // Compruebo que la tarea no ha sido transferida mas de 2 veces
+
+                        $veces = $this->getDoctrine()->getManager()->getRepository('App:Tareas')->createQueryBuilder('tareas')
+                            ->select('tareas.vecesTransferida')
+                            ->where('tareas.id = '.$request->attributes->get('id'))
+                            ->getQuery()->getResult();
+
+                        $veces = $veces[0]['vecesTransferida'];
+
+                        if ($veces < 2) {
+
+                            $sumarVez = $this->getDoctrine()->getManager()->getRepository('App:Tareas')->createQueryBuilder('tareas')
+                                ->update('App:Tareas', 'tareas')
+                                ->set('tareas.vecesTransferida', 'tareas.vecesTransferida + 1')
+                                ->where('tareas.id = ' .$request->attributes->get('id'))
+                                ->getQuery()->getResult();
+
+                            $cambiarTarea = $this->getDoctrine()->getManager()->getRepository('App:Tareas')->createQueryBuilder('tareas')
+                                ->update('App:Tareas', 'tareas')
+                                ->set('tareas.user', $id)
+                                ->where('tareas.id = ' . $request->attributes->get('id'))
+                                ->getQuery()->getResult();
+
+                            return $this->redirectToRoute('admin');
+
+
+                        } else {
+
+                            $form->addError(new FormError("La tarea ha sido transferida dos veces o más veces."));
+                        }
+
+                    } else {
+
+                        $form->addError(new FormError("La tarea está completada o el usuario tiene más de 3 tareas pendientes."));
+                    }
+
+                } else {
+
+                    $form->addError(new FormError("Usuario no encontrado."));
+
+                }
             }
         }
 
-        return $this->render('tranferir_tareas/index.html.twig', [
+        return $this->render('taskTransfer/taskTransfer.html.twig', [
             'taskTransferForm' => $form->createView(),
         ]);
     }
