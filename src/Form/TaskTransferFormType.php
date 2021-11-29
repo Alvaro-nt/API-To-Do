ee3d58ad99cf5627819c9f9c1594176e5bcfd325
Álvaro Miguel <alvaro.lorente@nodrizatech.com>
2021-11-29 11:21:33 +0100

    Add new Form to Transfer Tasks

--- src/Form/TaskTransferFormType.php
+++ src/Form/TaskTransferFormType.php
@@ -2,8 +2,8 @@
 
 namespace App\Form;
 
-use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +12,7 @@ class TaskTransferFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class, ['label' => 'Email del usuario que recibira la tarea: ']);
         ;
     }
 
